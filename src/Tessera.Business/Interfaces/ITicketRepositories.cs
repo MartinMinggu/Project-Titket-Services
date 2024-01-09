f7ba4772045d2372141f63a6cd3737c365137884
Martin Minggu <57125891+MartinMinggu@users.noreply.github.com>
2024-01-09 14:17:42 +0700

    Add files via upload
    File Add

---
+++ src/Tessera.Business/Interfaces/ITicketRepositories.cs
@@ -0,0 +1,12 @@
+﻿using Tessera.DataAccess.Models;
+
+namespace Tessera.Business.Interfaces;
+public interface ITicketRepositories
+{
+    void DeleteTicket(Ticket ticket);
+    bool FindTickteById(string id);
+    Ticket GetTicketById(string id);
+    List<Ticket> GetTicketPerPage(int pageNumber, int totalNumber);
+    Ticket InsertTicket(Ticket ticket);
+    Ticket UpdateTicket(Ticket ticket);
+}
---
+++ src/Tessera.Business/Repsitories/TicketRepositories.cs
@@ -0,0 +1,33 @@
+﻿using Tessera.Business.Interfaces;
+using Tessera.DataAccess.Models;
+
+namespace Tessera.Business.Repsitories;
+public class TicketRepositories : ITicketRepositories {
+    private static TesseraContext _tesseraContext;
+
+    public TicketRepositories(TesseraContext tesseraContext) {
+        _tesseraContext = tesseraContext;
+    }
+    public List<Ticket> GetTicketPerPage(int pageNumber, int totalNumber) {
+        return _tesseraContext.Tickets.Skip(pageNumber * totalNumber).Take(totalNumber).ToList();
+    }
+    public Ticket InsertTicket(Ticket ticket) {
+        _tesseraContext.Add(ticket);
+        return ticket;
+    }
+    public Ticket UpdateTicket(Ticket ticket) {
+        _tesseraContext.Add(ticket);
+        return ticket;
+    }
+    public void DeleteTicket(Ticket ticket) {
+        _tesseraContext.Remove(ticket);
+        _tesseraContext.SaveChanges();
+    }
+    public Ticket GetTicketById(string id) {
+        return _tesseraContext.Tickets.FirstOrDefault(t => t.Id == id);
+    }
+    public bool FindTickteById(string id) {
+        return _tesseraContext.Tickets.Any(t => t.Id == id);
+    }
+
+}
